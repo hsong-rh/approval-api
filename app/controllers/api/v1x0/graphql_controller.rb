47e8df28bcc59aced8a421c4d596953b078a4d69
hsong-rh <hsong@redhat.com>
2020-04-29 17:01:06 -0400

    refresh graphql context

--- app/controllers/api/v1x0/graphql_controller.rb
+++ app/controllers/api/v1x0/graphql_controller.rb
@@ -8,7 +8,10 @@ module Api
         variables = ::Insights::API::Common::GraphQL.ensure_hash(params[:variables])
         result = graphql_api_schema.execute(
           params[:query],
-          :variables => variables
+          :variables => variables,
+          :context   => {
+            :policy_scope_method => method(:policy_method)
+          }
         )
         render :json => result
       end
@@ -16,12 +19,17 @@ module Api
       def overlay
         {
           "^.*$" => {
-            "base_query" => lambda do |model_class, graphql_args, _ctx|
-              UserContext.current_user_context.graphql_params = graphql_args
-              policy_scope(model_class)
+            "base_query" => lambda do |model_class, graphql_args, ctx|
+              ps_method = ctx[:policy_scope_method]
+              ps_method.call(model_class, graphql_args)
             end
           }
         }
+      end
+
+      def policy_method(model_class, graphql_args)
+        UserContext.current_user_context.graphql_params = graphql_args
+        policy_scope(model_class)
       end
     end
   end
